6e9609b300118d373760488850e0f78a99d4fa12
Luka <odak.skywalker@gmail.com>
2020-04-01 14:33:21 +0200

    before angular update

--- moneyFlowFrontend/src/app/app.component.ts
+++ moneyFlowFrontend/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'moneyFlowFrontend';
+
+  events: string[] = [];
+  opened: boolean;
 }
--- moneyFlowFrontend/src/app/app.module.ts
+++ moneyFlowFrontend/src/app/app.module.ts
@@ -5,6 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 import { IncomeModule } from './income/income.module';
 import { OutcomeModule } from './outcome/outcome.module';
 import { FlowModule } from './flow/flow.module';
@@ -17,10 +31,16 @@ import { FlowModule } from './flow/flow.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     IncomeModule,
     OutcomeModule,
-    FlowModule
+    FlowModule,
+
+
+    MatSidenavModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
