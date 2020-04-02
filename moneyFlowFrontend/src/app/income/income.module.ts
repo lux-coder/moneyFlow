48620f555a44051b8200ea97c8bc5f7b3716be61
Luka <odak.skywalker@gmail.com>
2020-04-02 13:57:29 +0200

    basic mat table

--- moneyFlowFrontend/src/app/income/income.module.ts
+++ moneyFlowFrontend/src/app/income/income.module.ts
@@ -5,14 +5,19 @@ import { IncomeRoutingModule } from './income-routing.module';
 import { IncomeHomeComponent } from './income-home/income-home.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { TableComponent } from './table/table.component';
+
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [IncomeHomeComponent],
+  declarations: [IncomeHomeComponent, TableComponent],
   imports: [
     CommonModule,
     IncomeRoutingModule,
 
-    SharedModule
+    SharedModule,
+
+    MatTableModule
   ]
 })
 export class IncomeModule { }
